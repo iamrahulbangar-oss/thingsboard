6e8e57c06dfcb14e0899b47d7bedb52757f02884
Andrii Shvaika <ashvayka@thingsboard.io>
2026-04-02 09:27:51 +0300

    fix(iot-hub): fix device register endpoint JSON deserialization
    Accept JsonNode and use JacksonUtil.treeToValue() instead of direct
    @RequestBody DeviceInstalledItemDescriptor. The @JsonTypeInfo on the
    parent interface caused Spring's message converter to fail when
    deserializing the concrete class directly.

--- application/src/main/java/org/thingsboard/server/controller/IotHubController.java
+++ application/src/main/java/org/thingsboard/server/controller/IotHubController.java
@@ -31,6 +31,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import jakarta.servlet.http.HttpServletRequest;
 import org.thingsboard.server.common.data.exception.ThingsboardException;
+import org.thingsboard.common.util.JacksonUtil;
 import org.thingsboard.server.common.data.iot_hub.DeviceInstalledItemDescriptor;
 import org.thingsboard.server.common.data.iot_hub.IotHubInstalledItem;
 import org.thingsboard.server.common.data.page.PageData;
@@ -70,7 +71,8 @@ public class IotHubController extends BaseController {
     @ResponseBody
     public InstallItemVersionResult registerDeviceInstall(
             @RequestParam String versionId,
-            @RequestBody DeviceInstalledItemDescriptor descriptor) throws ThingsboardException {
+            @RequestBody JsonNode body) throws ThingsboardException {
+        DeviceInstalledItemDescriptor descriptor = JacksonUtil.treeToValue(body, DeviceInstalledItemDescriptor.class);
         return iotHubService.registerDeviceInstall(getCurrentUser(), versionId, descriptor);
     }
 
